2bfd096b9c1a5a2531d4ef6f5eb1623fab162de3
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-09-04 12:47:37 +0300

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,7 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
+require "active_support/inflector"
 
-class Song
-
-
+class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -11,34 +9,29 @@ class Song
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "pragma table_info('#{table_name}')"
+    sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
+
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
+
+    table_info.each { |row| column_names << row["name"] }
     column_names.compact
   end
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
+  # self.column_names.each { |col_name| attr_accessor col_name.to_sym }
 
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
-
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  def initialize(options = {})
+    pp options
+    options.each { |key, value| self.send("#{key}=", value) }
   end
 
   def table_name_for_insert
     self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if { |col| col == "id" }.join(", ")
   end
 
   def values_for_insert
@@ -49,16 +42,15 @@ class Song
     values.join(", ")
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  def save
+    DB[:conn].execute(
+      "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES(?)",
+      [values_for_insert]
+    )
+    row =
+      DB[:conn].execute(
+        "SELECT last_insert_rowid() FROM #{table_name_for_insert}"
+      )
+    @id = [0][0]
   end
-
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
-  end
-
 end
-
-
-
